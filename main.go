dd2fcc6f0a31ebcfe7a78c9f23de93fc8a003f35
Victor Romero <vcoromero@gmail.com>
2024-05-23 13:03:35 -0600

    feat: preparing to use lambdas

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world!!")
+}
+
+// func CallLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+// 	var res *events.APIGatewayProxyResponse
+
+// 	awsgo.InitializeAWS()
+
+// 	if !ValidatedParameters() {
+// 		res = &events.APIGatewayProxyResponse{
+// 			StatusCode: 400,
+// 			Body:       "Error en las variables de entorno, deben incluir 'SecretName', 'BucketName' y 'UrlPrefix'",
+// 			Headers: map[string]string{
+// 				"Content-type": "application/json",
+// 			},
+// 		}
+// 		return res, nil
+// 	}
+// }
+
+func ValidatedParameters() bool {
+	_, gotParameter := os.LookupEnv("SecretName")
+	if !gotParameter {
+		return gotParameter
+	}
+
+	_, gotParameter = os.LookupEnv("BucketName")
+	if !gotParameter {
+		return gotParameter
+	}
+
+	_, gotParameter = os.LookupEnv("UrlPrefix")
+	if !gotParameter {
+		return gotParameter
+	}
+	return gotParameter
 }
